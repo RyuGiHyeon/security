011b37d03783737ab43cb94335b42e3e0580d082
GitHyeon <ryuoo0123@gmail.com>
2024-09-06 14:59:27 +0900

    ex3

--- src/main/java/com/example/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/example/springsecurity/config/ProjectConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -12,17 +13,18 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class ProjectConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        var userDetailsService =
-                new InMemoryUserDetailsManager();
+        var userDetailsService = new InMemoryUserDetailsManager();
 
         var user = User.withUsername("john")
                 .password("12345")
                 .authorities("read")
                 .build();
+
         userDetailsService.createUser(user);
 
         return userDetailsService;
@@ -35,10 +37,8 @@ public class ProjectConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .httpBasic(Customizer.withDefaults())  // Deprecated 메서드 대신 사용
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll());
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
 
         return http.build();
     }
